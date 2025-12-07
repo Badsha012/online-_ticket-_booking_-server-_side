a78d65db2568443b501d89265994d705c4c73291
Badsha012 <badshasheikh361@gmail.com>
2025-12-07 15:57:12 +0600

    update

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/', (req, res) => {
 
 async function run() {
   try {
-    await client.connect();
+    //await client.connect();
 
     const db = client.db('Ticket-db');
     const ticketCollection = db.collection('Tickets');
