3f84736dbcecc6904645a3fdb72deb1fc377a2c7
Badsha012 <badshasheikh361@gmail.com>
2025-12-07 16:32:25 +0600

    six commit

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // CONNECT to DB
-    await client.connect();
+    //await client.connect();
 
     const db = client.db("Ticket-db");
     const ticketCollection = db.collection("Tickets");
